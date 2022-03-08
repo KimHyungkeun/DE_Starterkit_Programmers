65e4e578b4a2a41621dbcd40ae837f652d3b77b3
KimHyungkeun <hyungkeun.kim95@gmail.com>
2022-03-08 10:02:52 +0900

    week5_hw

--- week5_assignment_incermental_update.py
+++ week5_assignment_incermental_update.py
@@ -56,34 +56,25 @@ def load(**context):
     
     cur = get_Redshift_connection()
     lines = context["task_instance"].xcom_pull(key="return_value", task_ids="transform")
-
-    create_sql = """CREATE TABLE {schema}.{tmp} AS SELECT * FROM {schema}.{table};""".format(schema=schema, tmp = "temp_weather_forecast", table=table)
-    cur.execute(create_sql)
     
+    del_sql = """BEGIN;DELETE FROM {schema}.{table};""".format(schema=schema, table=table)
+    cur.execute(del_sql)
+
     n = len(lines)
     for i in range(n):
         if lines[i] != '' :
             (dt, day, min, max) = lines[i].split(",")
             print(dt, "|", day, "|", min, "|", max)
-            sql = """INSERT INTO {schema}.{tmp}} VALUES ('{dt}', {day}, {min}, {max});""".format(schema=schema, tmp = "temp_weather_forecast", dt=dt, day=day, min=min, max=max)
+            sql = """INSERT INTO {schema}.{table} VALUES ('{dt}', {day}, {min}, {max});""".format(schema=schema, table=table, dt=dt, day=day, min=min, max=max)
             print(sql)
             cur.execute(sql)
-    
-    
-    del_sql = """BEGIN;DELETE FROM {schema}.{table};""".format(schema=schema, table=table)
-    insert_sql = """INSERT INTO {schema}.{table} SELECT date, temp, min_temp, max_temp, created_date
-                    FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY date ORDER BY created_date DESC) 
-                    seq FROM {schema}.{tmp}) WHERE seq = 1;""".format(schema=schema, tmp = "temp_weather_forecast", table=table)
-    
-    cur.execute(del_sql)
-    cur.execute(insert_sql)
     cur.execute("END;")
 
 
 
 
-dag_full_refresh = DAG(
-    dag_id = 'full_refresh_assignment',
+dag_incremental_update = DAG(
+    dag_id = 'incremental_update_assignment',
     start_date = datetime(2022,3,7), # 날짜가 미래인 경우 실행이 안됨
     schedule_interval = '0 1 * * *',  # 적당히 조절
     catchup = False,
@@ -102,7 +93,7 @@ extract = PythonOperator(
         'url':  "https://api.openweathermap.org/data/2.5/onecall?lat=37.532600&lon=127.024612&exclude=current,minutely,hourly,alerts&appid="+Variable.get("open_weather_api_key")
     },
     provide_context=True,
-    dag = dag_full_refresh)
+    dag = dag_incremental_update)
 
 transform = PythonOperator(
     task_id = 'transform',
@@ -110,16 +101,16 @@ transform = PythonOperator(
     params = { 
     },  
     provide_context=True,
-    dag = dag_full_refresh)
+    dag = dag_incremental_update)
 
 load = PythonOperator(
     task_id = 'load',
     python_callable = load,
     params = {
         'schema': 'hyungkeun_kim95',
-        'table': 'weather_forecast'
+        'table': 'weather_forecast_upsert'
     },
     provide_context=True,
-    dag = dag_full_refresh)
+    dag = dag_incremental_update)
 
 extract >> transform >> load
